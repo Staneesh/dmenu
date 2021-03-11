6f4ff64119399ae6cb258a6817c6b189dfa201e5
Staneesh <blkreasons@outlook.com>
2021-03-11 18:21:57 +0100

    Most recent config

---
+++ config.h
@@ -0,0 +1,31 @@
+/* See LICENSE file for copyright and license details. */
+/* Default settings; can be overriden by command line. */
+
+static int instant = 0;
+static int topbar = 1;                      /* -b  option; if 0, dmenu appears at bottom     */
+static int fuzzy = 1;                      /* -F  option; if 0, dmenu doesn't use fuzzy matching     */
+/* -fn option overrides fonts[0]; default X11 font or font set */
+static const char *fonts[] = {
+	"libertine:size=16"
+};
+static const char *prompt      = NULL;      /* -p  option; prompt to the left of input field */
+static const char *colors[SchemeLast][2] = {
+	/*     fg         bg       */
+	[SchemeNorm] = { "#bbbbbb", "#002b36" },
+	[SchemeSel] = { "#bbbbbb", "#073642"},
+	[SchemeSelHighlight] = { "#00bb22", "#073642" },
+	[SchemeNormHighlight] = { "#00bb22", "#002b36" },
+	[SchemeBorderColor] = {"#00bb22", "#00bb22"},
+	[SchemeOut] = { "#000000", "#00bb22" },
+};
+/* -l option; if nonzero, dmenu uses vertical list with given number of lines */
+static unsigned int lines      = 10;
+
+/*
+ * Characters not considered part of a word while deleting words
+ * for example: " /?\"&[]"
+ */
+static const char worddelimiters[] = " ";
+
+/* Size of the window border */
+static const unsigned int border_width = 2;
